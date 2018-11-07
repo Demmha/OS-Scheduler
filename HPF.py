1e86e73608f788595de9c8f6882ed3d22a71012c
Demha <commandos_valentino@yahoo.com>
2018-11-07 16:01:31 +0200

    small fix in HPF

--- HPF.py
+++ HPF.py
@@ -1,5 +1,4 @@
 import numpy as np
-from collections import deque
 from copy import deepcopy
 
 def update_queue(process_list,process_queue,t): # list hya aslya w ally hy7t feha hya process_queue
@@ -28,19 +27,23 @@ for i in range (0,num):
 process_list=(sorted(process_list, key=lambda x: (-x[1],x[3]), reverse=True))
 # the program always start at arrival time of 1st process
 Current_Time = process_list[0][1]
+StartTime = np.zeros(num) #to keep track of start time of each process
+StartTime[0] = Current_Time
 Current_Process=np.zeros([num,4],dtype=float) # process sequence of execution
 process_queue=list()
-complete=0
-while(not(complete==num)):
-    process_queue = update_queue(process_list, process_queue, Current_Time)
-    if (not(len(process_queue)==0)):
-        Current_Process[complete]= deepcopy(process_queue[0])
-        print(Current_Process[complete])
-        process_queue.remove(process_queue[0])
-        Current_Time += Current_Process[complete][2]
-        complete+=1
+
+complete = 0
+while complete != num:
+    process_queue= update_queue(process_list, process_queue, Current_Time)
+    if(len(process_queue)==0):
+        Current_Time =  process_list[complete][1]  
     else:
-        Current_Time+=1
+        Current_Process.insert(complete, deepcopy(process_queue[0]))
+        process_queue.remove(process_queue[0])
+        StartTime[complete] = Current_Time
+        Current_Time += Current_Process[complete][2]
+        complete += 1
+
  # waiting time of each process
 WaitTime=np.zeros(num,dtype=float)
 # Turnaround time of each process
@@ -50,9 +53,9 @@ WaitedTurnaroundTime=np.zeros(num,dtype=float)
 #calculate Wait time ,turnaround time, weighted turnaround time
 for i in range (0,len(Current_Process)):
     if (not (i == 0)):  # as first process may not came at t=0 but whenever it come it will serve immediately
-        WaitTime[i] = WaitTime[i-1] + Current_Process[i - 1][2]- Current_Process[i][1]  # waiting time of previous one and burst time of it
-    if (WaitTime[i] < 0):  # as if a process came and processor was free so it will start immediatly after Context Switching time
-        WaitTime[i] = 0
+        WaitTime[i] = StartTime[i] - Current_Process[i][1]# wait time = start time - arrival time
+        if WaitTime[i] < 0: #if the current process arrived after the execution of the previous one
+            WaitTime[i] = 0
     WaitTime[i] += Context_Switch_Time
     TurnaroundTime[i]= WaitTime[i]+ Current_Process[i][2] # wait time+ burst time
     WaitedTurnaroundTime[i]=TurnaroundTime[i]/Current_Process[i][2] #turnaroundtime/burst time
@@ -61,7 +64,6 @@ AverageTurnaroundTime= np.sum(TurnaroundTime)/num
 #calculate Average weightturnaround time of schedule
 AverageWaitedTurnaroundTime= sum(WaitedTurnaroundTime)/num
 
-
 with open(outputfile, "w") as output:
     output.write('ProcessID\tWaitingTime\tTurnaroundTime\tWaitedturnaroundTime\n ')
     for i in range (0,num):
@@ -69,4 +71,3 @@ with open(outputfile, "w") as output:
     output.write("Average TurnaroundTime of Schedule = "+str(AverageTurnaroundTime)+ '\n ')
     output.write("Average WaitedTurnaroundTime of Schedule = "+str(AverageWaitedTurnaroundTime)+ '\n ')
     output.close()
-
