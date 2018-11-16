2a20f6476c67770f9c8c4002650a325d85482f23
Demha <commandos_valentino@yahoo.com>
2018-11-16 13:21:32 +0200

    srtf for now

--- SRTF.py
+++ SRTF.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from process import process
 from print_p import print_p
 from Draw_Graph import Draw_graph
+
 def update_queue(process_list,process_queue,t): # list hya aslya w ally hy7t feha hya process_queue
     for i in range (0,len(process_list)):
         if (((process_list[i].arrival)<=t) and (not(process_list[i].arrival)==-1)):
@@ -10,17 +11,6 @@ def update_queue(process_list,process_queue,t): # list hya aslya w ally hy7t feh
             process_list[i].arrival =-1 # flag to indicate that this process is already executed or listed
     process_queue.sort(key=lambda x: (x.remaining), reverse=False)
     return(process_queue)
-
-def get_data(inputfile,cst):
-    with open(inputfile, "r") as input:
-        in_arr = input.read().split()
-    process_list=list()
-    k=1
-    for i in range (0,len(in_arr)/4):
-        # process id     # process arrival time         # process brust time     # process priority
-        process_list.append(process(int(in_arr[k]),float(in_arr[k+1]),float(in_arr[k+2]), int(in_arr[k+3])))
-        k+=4
-    return (process_list)
 
 def SRTF(process_list,cst):
     num=len(process_list)
@@ -31,20 +21,19 @@ def SRTF(process_list,cst):
     Current_Time = process_list[0].arrival
     Current_Process=list()
     process_queue=list() # process sequence of execution
-    for i in range (0,len(process_list)):
-        print process_list[i].pid
+    
     complete=0
     i=0
     l_id=0
     s_t = np.array([-1 for i in range (0,num+1)])
     f_t = np.array([0 for i in range (0,num+1)])
     step=1
-    #assume in this algorithm check every 1 min
+    #assume in this algorithm check every 1 sec
     while complete != num :
         #print("at time = "+str(Current_Time))
         process_queue = update_queue(process_list,process_queue,Current_Time)
         #print("p" +str(process_queue[0].pid))
-        if len(process_queue)>0 :
+        if len(process_queue) > 0:
             if process_queue[0].pid != l_id : # check if process changed or not
                 Current_Time+= cst
                 if s_t[process_queue[0].pid]==-1: # check if this process start before or not
@@ -54,20 +43,34 @@ def SRTF(process_list,cst):
                 l_id = process_queue[0].pid
                 i+=1
 
-            process_queue[0].remaining-=step
-            if (process_queue[0].remaining <=0):
-                f_t[process_queue[0].pid] = Current_Time+1
-                complete+=1
+            if process_queue[0].remaining > step:
+                process_queue[0].remaining -= step
+                Current_Time += step
+            else:
+                Current_Time += process_queue[0].remaining
+                process_queue[0].remaining = 0
+                f_t[process_queue[0].pid] = Current_Time
+                Current_Process[i-1].finish = Current_Time
+                complete += 1
                 process_queue.remove(process_queue[0])
+        else:
+             Current_Time += step  
+#            process_queue[0].remaining-=step
+#            if (process_queue[0].remaining <=0):
+#                f_t[process_queue[0].pid] = Current_Time+1
+#                complete+=1
+#                process_queue.remove(process_queue[0])
 
-            Current_Process[len(Current_Process)-1].finish = Current_Time+1
-        Current_Time+=step
+#            Current_Process[len(Current_Process)-1].finish = Current_Time+1
+#        Current_Time+=step
 
     avg_tat=0
     avg_wtat=0
     for i in range (0,len(process_list)) :
-        process_list[i].wait = f_t[process_list[i].pid] - s_t[process_list[i].pid]  # start - finish
-        process_list[i].tat= process_list[i].wait+ process_list[i].running # wait time+ burst time
+        process_list[i].tat = f_t[process_list[i].pid] - process_list[i].copy_arrival
+        process_list[i].wait = process_list[i].tat - process_list[i].running
+#        process_list[i].wait = f_t[process_list[i].pid] - s_t[process_list[i].pid]  # start - finish
+#        process_list[i].tat= process_list[i].wait+ process_list[i].running # wait time+ burst time
         process_list[i].wtat= process_list[i].tat/ process_list[i].running #turnaroundtime/burst time
         avg_tat+=process_list[i].tat
         avg_wtat+=process_list[i].wtat
@@ -80,6 +83,14 @@ def SRTF(process_list,cst):
     print_p(process_list, avg_tat, avg_wtat, "SRTF")
     return(Current_Process)
 
-p = get_data("3",1)
-List = SRTF(p,1)
+pList = []
+
+pList.append(process(1, 0, 4, 0))
+pList.append(process(2, 1, 5, 0))
+pList.append(process(3, 2, 2, 0))
+pList.append(process(4, 3, 1, 0))
+pList.append(process(5, 5, 6, 0))
+
+
+List = SRTF(pList, 1)
 Draw_graph(List)
