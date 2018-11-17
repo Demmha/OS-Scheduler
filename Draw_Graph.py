a31ad6d880e1b70416caf23b695b39c4759c1b43
Reem Ashraf <remoash976@gmail.com>
2018-11-17 10:03:46 +0200

    small change in GUI

--- Draw_Graph.py
+++ Draw_Graph.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
 from process import process
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-def Draw_graph (process_list):
+def Draw_graph (process_list,root):
         S_T=list()
         D_T=list()
         I_D=list()
@@ -10,15 +11,15 @@ def Draw_graph (process_list):
                 I_D.append(elem.pid)
                 D_T.append((elem.finish-elem.start))
         # plotting a bar chart
-        plt.bar(S_T, I_D,
-                width = D_T, color = ['red', 'blue'], align="edge")
+        figure1 = Figure(figsize=(5, 4),dpi=100)
+        subplot1 = figure1.add_subplot(111)
+        subplot1.set_title(" graph")
+        subplot1.set_xlabel(" Time")
+        subplot1.set_ylabel(" Process_ID")
+        subplot1.bar(S_T, I_D,width=D_T ,color='g', align="edge")
+        bar1 = FigureCanvasTkAgg(figure1, root)
 
-        # naming the x-axis
-        plt.xlabel(' Time ')
-        # naming the y-axis
-        plt.ylabel(' Process Number ')
-        # plot title
-        plt.title(' Scheduling processes ')
+        return (bar1)
 
-        # function to show the plot
-        plt.show()
+
+
--- GUI.py
+++ GUI.py
@@ -6,15 +6,17 @@ from FCFS import FCFS
 from RR import RR
 from SRTF import SRTF
 from Draw_Graph import Draw_graph
+from tkinter import S as nw
 
 def onvalue_change(self):
     if algo.get() == OPTIONS[2]:
         L4.pack()
-        E4.pack()
+        E4.pack(anchor=nw)
     else:
         L4.pack_forget()
         E4.pack_forget()
-        
+
+
 def val_(val):
     try:
         val = float(val)
@@ -23,7 +25,7 @@ def val_(val):
         return 1
     except ValueError:
         return 0
-    
+
 
 def validate():
     try:
@@ -35,9 +37,11 @@ def validate():
             raise ValueError("Enter a reasonable Time Quantum")
         return True
     except Exception as error:
-            messagebox.showerror("Error", repr(error))
-            return False
-        
+        messagebox.showerror("Error", repr(error))
+        return False
+
+
+
 def run():
     if not validate():
         return
@@ -49,36 +53,74 @@ def run():
     if algo.get() == OPTIONS[2]:
         List = RR(pList, float(t2.get()), float(t1.get()))
     if algo.get() == OPTIONS[3]:
-        List = STRF(pList, float(t1.get()))
-    Draw_graph(List)
+        List = SRTF(pList, float(t1.get()))
+    bar1=Draw_graph(List,canvas1)
+    bar1.get_tk_widget().pack(fill=BOTH, expand=1)
+def reset():
+    List=list()
+    bar1=bar1=Draw_graph(List,canvas1)
+    bar1.get_tk_widget().pack(fill=BOTH, expand=1)
+
 
 window = Tk()
 window.title("Os scheduler")
-window.geometry("300x250")
+window.geometry("700x300")
+canvas1 = Canvas(window, width=300, height=300)
+canvas1.pack(side=RIGHT )
 
-L1 = Label(window, text="Input File Name").pack()
+L1 = Label(window,pady=10,text="Input File Name").pack(anchor=nw)
 
 filename = StringVar()
-E1 = Entry(window, textvariable = filename).pack()
+E1 = Entry(window, textvariable = filename).pack(anchor=nw)
 
-L2 = Label(window, text="Choose Algorithm").pack()
+L2 = Label(window,pady=10, text="Choose Algorithm").pack(anchor=nw)
 
 OPTIONS = ["HPF", "FCFS", "RR", "SRTN"]
-
+#############################################################33
 algo = StringVar(window)
 algo.set(OPTIONS[0])
 
-algolist = OptionMenu(window, algo, *OPTIONS, command = onvalue_change).pack()
+algolist = OptionMenu(window, algo, *OPTIONS, command = onvalue_change).pack(anchor=nw)
+#################################################################
+'''
+v=IntVar()
 
-L3 = Label(window, text="Context Switching Time").pack()
+frame=Frame(window)
+frame.pack()
+Radiobutton(frame,
+              text="HPF",
+              padx = 10,
+              variable=v,
+              value=1).pack(anchor=W,side=LEFT)
+Radiobutton(frame,
+              text="FCFS",
+              padx = 10,
+              variable=v,
+              value=2).pack(anchor=W,side=LEFT)
+Radiobutton(frame,
+              text="RR",
+              padx = 10,
+              variable=v,
+              value=3).pack(anchor=W,side=LEFT)
+Radiobutton(frame,
+              text="SRTF",
+              padx = 10,
+              variable=v,
+              value=4).pack(anchor=W,side=LEFT)
+'''
+########################################################33
+L3 = Label(window,pady=10, text="Context Switching Time").pack()
 
 t1 = StringVar()
-E2 = Entry(window, textvariable = t1).pack()
+E2 = Entry(window, textvariable = t1).pack(anchor=nw)
 
 t2 = StringVar()
-L4 = Label(window, text="Time Quantum")
-E4 = Entry(window, textvariable = t2)
+L4 = Label(window,pady=10, text="Time Quantum")
+E4 = Entry(window,textvariable = t2)
 
-b1 = Button(window, text ="Done", command = run).pack(side = BOTTOM)
+b1 = Button(relief= GROOVE,text ="DONE",padx=10,command = run).pack(anchor=nw)
 
+
+#b2 = Button(relief= GROOVE,text ="RESET",padx=10,justify=LEFT,command = reset).pack()
+#canvas1.create_window(200,320,window=b2)
 window.mainloop()
